8cc0b629c5adfbde17b92e2aa033e0de4d4001b6
titoOgarcia <47427293+titoOgarcia@users.noreply.github.com>
2019-04-23 12:30:20 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloWorld{
+   public static void main(String[] args){
+      JFrame frame = new JFrame("JFrame Example");
+      frame.setSize(600,400);
+   }
+}
